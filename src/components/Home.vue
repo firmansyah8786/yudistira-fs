2279bf3d80c4958795b8862a89fdb33146ee318c
Yudistira Firman Syah <firmansyah8786@gmail.com>
2021-10-30 19:53:04 +0700

    Added personal info

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -21,6 +21,20 @@
                 </a>
             </div>
         </section>
+        <section class="light-bg">
+            <div class="text-block">
+                <h1 class="text-center mb-3">Hi, My name is Yudistira Firman Syah</h1>
+                <p>
+                    I am a backend PHP web developer, but I can also do full-stack development. I have 2 years experience in web-development. My current interest is JavaScript web development, because in my previous positions I needed to use PHP based framework. I also currently learning about advanced front-end development. This website is built using Vue 3 as my learning experience.
+                </p>
+                <p>
+                    <strong>PHP</strong> is my favorite for creating APIs, especially using <strong>Laravel</strong> framework. For front-end, I've used <strong>blade</strong>, <strong>ejs</strong>, and <strong>vue</strong> so far. I can also do integration with external API in your website.
+                </p>
+                <p>
+                    I've worked remotely with fixed working hours and flexible hours before. So I can adapt to various working hours policy. I can also do freelance job with reasonably fast progress.
+                </p>
+            </div>
+        </section>
     </main>
 </template>
 <script>
@@ -63,4 +77,8 @@
     .links{
         height: 18vh;
     }
+    .text-block{
+        padding: 1rem 15% 0;
+    }
+
 </style>
